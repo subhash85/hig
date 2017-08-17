73a4ee709f7cf187bed38d720bb52065346aeb4a
Sean Durham <seandurham@novc4zqk12.ads.autodesk.com>
2017-08-16 17:23:42 -0700

    Adjust spacing of items in the top nav

--- src/implementations/vanilla/src/components/global-nav/top-nav/shortcut/shortcut.js
+++ src/implementations/vanilla/src/components/global-nav/top-nav/shortcut/shortcut.js
@@ -1,8 +1,8 @@
-import './shortcut.scss';
+import "./shortcut.scss";
 
-var Template = require('./shortcut.html');
-var Interface = require('interface.json');
-var Core = require('_core.js');
+var Template = require("./shortcut.html");
+var Interface = require("interface.json");
+var Core = require("_core.js");
 
 /**
  * Creates an Shortcut
@@ -11,36 +11,40 @@ var Core = require('_core.js');
  */
 
 class Shortcut extends Core {
+  constructor(options) {
+    super(options);
+    this._render(Template, options);
+  }
 
-    constructor(options){
-        super(options);
-        this._render(Template, options);
-    }
+  onClick(fn) {
+    return this._attachListener("click", this.el, this.el, fn);
+  }
 
-    onClick(fn){
-        return this._attachListener("click", this.el, this.el, fn);
-    }
+  setIcon(icon) {
+    this._findDOMEl(
+      ".hig__global-nav__top-nav__shortcut__link__icon",
+      this.el
+    ).innerHTML = this._getIconString(icon);
+  }
 
-    setIcon(icon){
-        this._findDOMEl(".hig__global-nav__top-nav__shortcut__link__icon", this.el).innerHTML = this._getIconString(icon);
-    }
+  setTitle(title) {
+    this.el.textContent = title;
+    this.el.setAttribute("title", title);
+  }
 
-    setTitle(title){
-        this.el.textContent = title;
-        this.el.setAttribute("title", title);
-    }
-
-    setLink(link){
-        this.el.setAttribute("href", link);
-    }
-
+  setLink(link) {
+    this.el.setAttribute("href", link);
+  }
 }
 
-Shortcut._interface = Interface['components']['GlobalNav']['partials']['TopNav']['partials']['Shortcut'];
+Shortcut._interface =
+  Interface["components"]["GlobalNav"]["partials"]["TopNav"]["partials"][
+    "Shortcut"
+  ];
 Shortcut._defaults = {
-    icon: "",
-    title: "",
-    link: "#"
+  icon: "",
+  title: "",
+  link: "#"
 };
 
 module.exports = Shortcut;
